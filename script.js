ca2fc51fa7a38416a06717405bd1845b595d2e02
KrisW9001 <link3563@gmail.com>
2023-04-11 12:24:41 -0700

    home is update 1

---
+++ script.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const sections = document.querySelectorAll('section')
+    let mode = 'dark';
+
+    button.addEventListener('click', function() {
+        if (mode === 'dark') {
+            body.className = 'switch';
+            button.className = 'switch';
+            for (const section of sections) {
+                section.className = 'switch';
+            }
+            mode = 'light';
+        } else {
+            body.removeAttribute('class');
+            button.removeAttribute('class');
+            for (const section of sections) {
+                section.removeAttribute('class');
+            }
+            mode = 'dark';
+        }
+    })
+})()
