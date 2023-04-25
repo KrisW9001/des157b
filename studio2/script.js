7a55d42e2df59cfcd9554c678cab1c8cb8d66c56
KrisW9001 <link3563@gmail.com>
2023-04-25 16:30:59 -0700

    studio 2 WIP

--- studio2/script.js
+++ studio2/script.js
@@ -1,10 +1,28 @@
-(function() {
+
     'use strict';
     console.log("reading js");
 
     let globalData;
 
+    async function getData(){
+        const myMusic = await fetch ('data/music.json');
+        const data = await myMusic.json();
+        console.log(data);
+        globalData = data;
+        document.querySelector('#music').innerHTML = outputHTML1(data);
+    }
 
+    function outputHTML1(data){
+        let html = '<option>---</option>';
+        const dataPoints = Object.keys(data);
+        console.log(dataPoints);
+        dataPoints.forEach(function(eachPoint){
+            html += `<p class="bubble" id="${data[eachPoint].from}">`;
+            html += `${data[eachPoint].name}`;
+            html += '</p>';
+        });
+        return html;
+    }
 
+    getData();
 
-})()
