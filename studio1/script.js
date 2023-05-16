4b3b2cdfc842b1af8f2ef1b4c21de75c681f444f
KrisW9001 <link3563@gmail.com>
2023-05-16 16:17:16 -0700

    studio 1 partial update

--- studio1/script.js
+++ studio1/script.js
@@ -4,19 +4,29 @@
 
     const vidorder = ['media/part1.mp4', 'media/part2.mp4', 'media/part3.mp4', 'media/part4.mp4', 'media/part5.mp4', 'media/part6.mp4', 'media/part7.mp4', 'media/part8.mp4', 'media/part9.mp4'];
     let currentvid = 0;
-    const media = document.querySelector('video');
+    let media = document.querySelector('video');
+    let vidsource = document.querySelector('#source').getAttribute('src');
     // let vidsource = document.querySelector('#source');
 
     // vidsource = vidorder[currentvid];
 
+    
     document.querySelector('#next').addEventListener('click', function(event){
-        event.preventDefault();
-        currentvid++;
-        document.querySelector('#source').innerHTML(`id="source" src="${vidorder[currentvid]}" type="video/mp4"`);
-        console.log(currentvid);
-        media.play();
+            event.preventDefault();
+            currentvid++;
+            document.querySelector('#source').src = vidorder[currentvid];
+            console.log(currentvid);
+            console.log(vidorder[currentvid]);
+            media.play();
     })
 
-
-
+    document.querySelector('#back').addEventListener('click', function(event){
+        event.preventDefault();
+        currentvid--;
+        document.querySelector('#source').src = vidorder[currentvid];
+        console.log(currentvid);
+        console.log(vidorder[currentvid]);
+        media.play();
+    })
+    
 })()
