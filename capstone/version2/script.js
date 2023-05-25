4544fb73347e083a996f2bfdebd5ce97ba87c66d
KrisW9001 <link3563@gmail.com>
2023-05-25 16:28:21 -0700

    version 2 WIP 2

--- capstone/version2/script.js
+++ capstone/version2/script.js
@@ -1,11 +1,43 @@
 (function(){
     "use strict";
+    console.log('reading js');
 
     const button = document.querySelector('button');
     let piece = document.getElementById('piece');
     let para1 = document.getElementById('para1');
     let para2 = document.getElementById('para2');
     let title = document.getElementById('piecename');
+    let main = document.getElementsByTagName('main');
+    let section = document.getElementsByTagName('section');
 
     //defining values of each image map element to make later code shorter
+
+    const birds = document.getElementById('birds');
+    const birds2 = document.getElementById('birds2');
+    const market = document.getElementById('market');
+    const huynh = document.getElementById('hyunh');
+    const selma = document.getElementById('selma');
+    const farm = document.getElementById('farm');
+    const townname = document.getElementById('townname');
+    const patwin = document.getElementById('patwin');
+    const patwin2 = document.getElementById('patwin2');
+    const map = document.getElementById('map');
+    const business = document.getElementById('map');
+    const university = document.getElementById('university');
+    const civengage = document.getElementById('civicengagement');
+    const children = document.getElementById('children');
+    const agriculture = document.getElementById('agriculture');
+    const finalring = document.getElementById('finalring');
+    const finalring2 = document.getElementById('finalring2');
+
+    birds.addEventListener('click', birdpage);
+
+    function birdpage(){
+        birdpage.preventDefault();
+        document.getElementsByTagName('main').setAttribute('class', 'hidden');
+        console.log('main will now be invisible');
+        document.getElementsByTagName('section').className = 'visible';
+    }
+
+//code does not work, page refreshes and does not change visibility
 })()
