7486197df505bb06ea15ba7987154803c5c38fee
KrisW9001 <link3563@gmail.com>
2023-05-25 15:10:10 -0700

    version 2 WIP 1

---
+++ capstone/version2/script.js
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+
+    const button = document.querySelector('button');
+    let piece = document.getElementById('piece');
+    let para1 = document.getElementById('para1');
+    let para2 = document.getElementById('para2');
+    let title = document.getElementById('piecename');
+
+    //defining values of each image map element to make later code shorter
+})()
